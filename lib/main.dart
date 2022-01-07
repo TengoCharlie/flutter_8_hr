05eda35af14c7c4fc21b863e452e12497fed2cbe
Harsh Tenguriya <thegreatharsh1@gmail.com>
2022-01-08 03:39:43 +0530

    Day 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/home_page.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
 
 void main() {
   runApp(const MyApp());
+  // var c1 = const MyApp();
+  // print(c1.bringVegetables(thaila: true));
 }
 
 class MyApp extends StatelessWidget {
@@ -10,13 +13,34 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //context is a peice of info that which is where(location of widget)
     double pi = 3.14;
     bool isMale = true;
     num temp = 30.5;
     var forAnyVaariable = "tuesday";
     const pie = 3.14;
     final pies = 3.14;
-
-    return MaterialApp(home: HomePage());
+    // print(bringVegetables(thaila: false));
+    return MaterialApp(
+      // home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.light,
+        // primarySwatch: Colors.blueGrey,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
+    );
   }
+
+  // bringVegetables({required bool thaila, int rupees = 100}) {
+  //   return rupees;
+  // }
 }
